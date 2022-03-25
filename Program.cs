718a9e833b69414b0be6dca44bd299cea8f01eb4
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-25 20:39:36 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Cazuza - Exagerado");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Amor da minha vida");
+Console.WriteLine("Daqui até a eternidade");
+Console.WriteLine("Nossos destinos foram traçados na maternidade");
+Console.WriteLine();
+
+Console.WriteLine("Paixão cruel, desenfreada");
+Console.WriteLine("Te trago mil rosas roubadas");
+Console.WriteLine("Pra desculpar minhas mentiras, minhas mancadas");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Exagerado");
+Console.WriteLine("Jogado aos teus pés, eu sou mesmo exagerado");
+Console.WriteLine("Adoro um amor inventado");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Eu nunca mais vou respirar");
+Console.WriteLine("Se você não me notar");
+Console.WriteLine("Eu posso até morrer de fome");
+Console.WriteLine("Se você não me amar");
+Console.WriteLine();
+
+Console.WriteLine("Por você eu largo tudo");
+Console.WriteLine("Vou mendigar, roubar, matar");
+Console.WriteLine("Até nas coisas mais banais");
+Console.WriteLine("Pra mim é tudo ou nunca mais");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Exagerado");
+Console.WriteLine("Jogado aos teus pés, eu sou mesmo exagerado");
+Console.WriteLine("Adoro um amor inventado");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("E por você eu largo tudo");
+Console.WriteLine("Carreira, dinheiro, canudo");
+Console.WriteLine("Até nas coisas mais banais");
+Console.WriteLine("Pra mim é tudo ou nunca mais");
+Console.WriteLine();
+
+Console.WriteLine("Exagerado");
+Console.WriteLine("Jogado aos teus pés, eu sou mesmo exagerado");
+Console.WriteLine("Adoro um adoro um amor inventado");
+Console.WriteLine("Jogado aos teus pés com mil rosas roubadas");
+Console.WriteLine();
+
+Console.WriteLine("Exagerado");
+Console.WriteLine("Eu adoro um amor inventado");
+Console.WriteLine("Jogado aos teus pés, baby baby");
+Console.WriteLine("Eu sou mesmo exagerado");
+Console.WriteLine("Adoro um amor inventado");
